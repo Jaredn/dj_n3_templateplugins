d0148b932ab1835660604e5c1d8844fd0968a78e
Jared Newell <jnewell@box.com>
2017-09-27 10:15:20 -0700

    context at plugin level

--- dj_n3_templateplugins/templatetags/render_plugin.py
+++ dj_n3_templateplugins/templatetags/render_plugin.py
@@ -9,5 +9,7 @@ register = Library()
 @register.simple_tag(takes_context=True)
 def render_plugin(context, plugin_obj):
     plugin_instance = get_plugin_instance(plugin_obj)
+    plugin_context = plugin_instance.get_context_data()
+    context['plugin_local'][plugin_obj.name] = plugin_context
     t = Template(plugin_instance.render_html())
     return t.render(Context(context))
